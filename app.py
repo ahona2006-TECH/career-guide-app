281cc21b6f3d320089807bbe787f2f6b5106fd18
ahona2006-TECH <ahonazahir2006@gmail.com>
2026-05-30 19:28:03 +0600

    Fix Flask config for Render deployment

--- app.py
+++ app.py
@@ -264,5 +264,6 @@ def build_cv():
     return jsonify({"cv_data": data})
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
